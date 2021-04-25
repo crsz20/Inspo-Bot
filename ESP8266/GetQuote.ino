6855cd480a4a0f58566924cb5bf1554f91f26951
crsz20 <cris14.alex@gmail.com>
2021-04-25 00:30:45 -0500

    Successful JSON to String conversion of quote and source

--- ESP8266/GetQuote.ino
+++ ESP8266/GetQuote.ino
@@ -1,19 +1,13 @@
-/**
-   BasicHTTPSClient.ino
-
-    Created on: 20.08.2018
-
-*/
-
 #include <Arduino.h>
+
+#include <ArduinoJson.h>
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-
 #include <WiFiClientSecureBearSSL.h>
 const uint8_t fingerprint[20] = {0x2e, 0x3b, 0xd8, 0xe7, 0x5a, 0x6d, 0xe3, 0xe3, 0xfb, 0x81, 0x1e, 0xa6, 0x4b, 0xa4, 0x00, 0x1c, 0x1b, 0xeb, 0xca, 0x2a};
+
 
 
 ESP8266WiFiMulti WiFiMulti;
@@ -36,6 +30,35 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("SSID", "PASSWORD");
 }
+
+
+
+void parseJson(String json) {
+  
+  // Allocate the JSON document
+  const int capacity = JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(1)+JSON_OBJECT_SIZE(256);
+  DynamicJsonDocument doc(capacity);
+
+  //Parse
+  DeserializationError error = deserializeJson(doc, json);
+  
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  String quote = doc["quote"]["quote"];
+  String source = doc["quote"]["source"];
+
+  Serial.print("Quote: ");
+  Serial.println(quote);
+  Serial.print("Source: ");
+  Serial.println(source);
+}
+
+
+
 
 void loop() {
   // wait for WiFi connection
@@ -68,6 +91,7 @@ void loop() {
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
           String payload = https.getString();
           Serial.println(payload);
+          parseJson(payload);
         }
       } else {
         Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
